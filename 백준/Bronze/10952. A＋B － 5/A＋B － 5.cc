638f31e02c24671bf918da8d4067246a7a62c751
TaesubLee <162679741+TaesubLee@users.noreply.github.com>
2025-02-02 02:28:54 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+	ios_base::sync_with_stdio(false);
+	
+	int A, B;
+
+
+	while (1)
+	{
+		cin >> A >> B;
+		if (A == 0 && B == 0)
+		{
+			break;
+		}
+		cout << A + B << "\n";
+	}
+	return 0;
+}
