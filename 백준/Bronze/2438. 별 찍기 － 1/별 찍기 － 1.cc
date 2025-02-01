1f9d8d0717ea50b60ab56f36995fad64018ef643
TaesubLee <162679741+TaesubLee@users.noreply.github.com>
2025-02-02 02:53:56 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
