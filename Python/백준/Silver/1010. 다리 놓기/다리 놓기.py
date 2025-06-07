d560173665af54b0b3a7f78d271a98b2756c0420
TaesubLee <162679741+TaesubLee@users.noreply.github.com>
2025-06-07 17:00:51 +0900

    Delete Python/백준/Silver directory

--- Python/백준/Silver/1010. 다리 놓기/다리 놓기.py
+++
@@ -1,7 +0,0 @@
-import math
-def Combination(N, M):
-    return math.factorial(M) // (math.factorial(M-N) * math.factorial(N))
-TestCase = int(input())
-for i in range(TestCase):
-    N, M = map(int,input().split())
-    print(Combination(N,M))
