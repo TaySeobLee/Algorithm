3b59db248f0e80e3cb4e19133f39887e9445e51f
TayseobLee <162679741+TaySeobLee@users.noreply.github.com>
2025-06-29 09:56:03 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,57 +1,36 @@
-#include <iostream>
-
+#include<iostream>
+#include<vector>
 using namespace std;
-
-void QuickSort(int* array, int start, int end) {
-	if (start >= end) // 원소가 1개인 경우 그대로 두기
-		return;
-
-	int pivot = start; // 피봇은 첫번째 원소
-	int i = start + 1, j = end, temp;
-
-	while (i <= j) { // 엇갈릴 때까지 반복
-		while (i <= end && (array[i] <= array[pivot])) { // 피봇보다 큰 값을 찾을 때까지
-			i++;
-		}
-		while (j > start && (array[j] >= array[pivot])) { // 피봇보다 작은 값을 찾을 때까지
-			j--;
-		}
-
-		if (i > j) { // 엇갈린 상태. 피봇보다 작은 값과 피봇과 교체
-			temp = array[j];
-			array[j] = array[pivot];
-			array[pivot] = temp;
-		}
-		else { // 엇갈리지 않았다면 i와 j를 교체
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-		}
-	}
-
-	QuickSort(array, start, j - 1);
-	QuickSort(array, j + 1, end);
-}
 
 int main()
 {
-    int number_amount;
-    cin >> number_amount;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
-    int *number_array = new int[number_amount];
-    //array input
-    for(int i = 0; i < number_amount; i++)
+    int N;
+    cin >> N;
+    vector<int> A(N,0);
+
+    for(int i = 0; i<N; i++)
     {
-        cin >> number_array[i];
+        cin>>A[i];
     }
-    
-    //sorting
-    QuickSort(number_array, 0,number_amount-1);
-    for(int i = 0; i < number_amount ; i++)
+
+    for(int i = 0; i < N-1; i++)
     {
-        cout << number_array[i] <<endl;
+        for(int j = 0; j < N-1-i; j++)
+        {
+            if(A[j]>A[j+1])
+            {
+                int temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+            }
+        }
     }
-    
-    delete[] number_array;    
-    return 0;
+    for(int i = 0; i < N; i++)
+    {
+        cout << A[i] <<"\n";
+    }
 }
