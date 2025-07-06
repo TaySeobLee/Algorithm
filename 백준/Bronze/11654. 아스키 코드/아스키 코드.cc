49def8efb11025ed7d3ba5a509e5435699d9a86c
TayseobLee <162679741+TaySeobLee@users.noreply.github.com>
2025-07-06 09:49:01 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    char N;
+    cin >> N;
+    cout << (int)N << "\n";
+
+}
