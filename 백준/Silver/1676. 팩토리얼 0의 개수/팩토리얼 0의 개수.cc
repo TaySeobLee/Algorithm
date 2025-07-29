a5283922d06aa04c927cb4c47fd30afac5a7150c
TayseobLee <162679741+TaySeobLee@users.noreply.github.com>
2025-07-29 19:13:34 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int count = 0;
+    for (int i = 5; N / i >= 1; i *= 5) {
+        count += N / i;
+    }
+
+    cout << count << '\n';
+
+    
+}
